e6ec6777f004f1f6585618e38cfd28a9697ff39d
Stefan de Raadt <stefanderaadt@gmail.com>
2019-06-07 11:06:37 +0200

    Added base classes

--- src/main/java/org/nl/hu/sie/bep/adapter/IEFGenerator.java
+++ src/main/java/org/nl/hu/sie/bep/adapter/IEFGenerator.java
@@ -1,8 +1,14 @@
 package org.nl.hu.sie.bep.adapter;
 
+import org.nl.hu.sie.bep.dto.Row;
+import org.nl.hu.sie.bep.factories.IEFFactory;
+
 public class IEFGenerator implements IEFGeneratorAdapter {
 
     public void generate(Company company) {
+        Row bedrijfsRow = IEFFactory.create(company);
 
+        SaverAdapter saverAdapter = new Saver();
+        saverAdapter.save(bedrijfsRow);
     }
 }
--- src/main/java/org/nl/hu/sie/bep/factories/IEFFactory.java
+++ src/main/java/org/nl/hu/sie/bep/factories/IEFFactory.java
@@ -1,5 +1,9 @@
 package org.nl.hu.sie.bep.factories;
 
+import org.nl.hu.sie.bep.dto.Row;
+
 public class IEFFactory {
-    public Row generateIEF
+    public static Row create(Company company){
+
+    }
 }
